bc58ddba934aa618e08a56c9d16c0ddcebc8455e
dhruvagarwal <dhruvagga@gmail.com>
2014-04-02 00:37:45 +0530

    Update tweets_controller.rb

--- tweet_fetcher/app/controllers/tweets_controller.rb
+++ tweet_fetcher/app/controllers/tweets_controller.rb
@@ -2,10 +2,10 @@ require 'twitter'
 class TweetsController < ApplicationController
   def index
   	client = Twitter::REST::Client.new do |config|
-		config.consumer_key = 'quggZEzEtmj36RQdyf97sUcNZ'
-		config.consumer_secret = '7LNSr7B7LpN2ak1WPV83dmo9J453c2jnRYznqy1GVd2VDzme5p'
-		config.access_token = '2416338816-xAVrVBpdtjvStiXoqUHPTHDu1y1sPf7rJ8p0R4z'
-		config.access_token_secret = 'GTAI52goIwyoUCKci4l9AimKKaIfCK68J1mGp4wSiDKBL'
+		config.consumer_key = ''
+		config.consumer_secret = ''
+		config.access_token = ''
+		config.access_token_secret = ''
 	end
 	@keyword="watching"
 	@tweet=client.search(@keyword).take(100) #, :result_type => "recent").take(20)
